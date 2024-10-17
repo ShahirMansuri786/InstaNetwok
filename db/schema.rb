cd26569980ba2d68edec3711a426a69290d54524
shahir <shahirmansuri86@gmail.com>
2024-10-17 10:55:00 +0530

    AddSomeChangesForDeployement

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_09_03_064416) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -41,7 +44,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_03_064416) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment_text"
-    t.integer "post_id"
+    t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
@@ -50,7 +53,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_03_064416) do
 
   create_table "likes", force: :cascade do |t|
     t.string "likeable_type", null: false
-    t.integer "likeable_id", null: false
+    t.bigint "likeable_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
@@ -59,8 +62,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_03_064416) do
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
-    t.integer "user_id", null: false
-    t.integer "room_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "room_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["room_id"], name: "index_messages_on_room_id"
@@ -68,7 +71,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_03_064416) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.integer "user_id"
+    t.bigint "user_id"
     t.string "caption"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -92,14 +95,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_03_064416) do
 
   create_table "user_infos", force: :cascade do |t|
     t.string "bio"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_user_infos_on_user_id"
   end
 
   create_table "user_profiles", force: :cascade do |t|
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_user_profiles_on_user_id"
